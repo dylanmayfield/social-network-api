06f7d0945764e4dab821eb391b3986a15dec6f22
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-17 17:28:05 -0600

    fixed bug with in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+
 const moment = require('moment');
 
 const ReactionSchema = new Schema(
@@ -19,7 +20,7 @@ const ReactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: new Date(),
-            get: createdAtVal => dateFormat(createdAtVal)
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         }
     },
     {
@@ -40,7 +41,7 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         },
         username: {
             type: String,
