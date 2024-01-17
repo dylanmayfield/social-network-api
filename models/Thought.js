6253728c32fa57d0cc3fa882b8a2af460de3a908
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-17 15:31:31 -0600

    fixed bug in thoughts route

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [ReactionSchema]
-        
-    },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-);
-
-const ReactionSchema = new Schema(
+const reactionSchema = new Schema(
     {
         reactionId: {
             type: Schema.Types.ObjectId,
@@ -54,6 +27,36 @@ const ReactionSchema = new Schema(
         }
     }
 );
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]   
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
