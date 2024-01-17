ad8a186939534e15af7a42a48532167967e75bdc
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-17 14:55:19 -0600

    created thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThought).post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
