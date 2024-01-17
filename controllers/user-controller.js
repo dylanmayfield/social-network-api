ba94a0fe28b9a2987507181b4917ec97264a4b1d
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-17 15:36:47 -0600

    debugged user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,6 +8,10 @@ const userController = {
             path: 'thoughts',
             select: '-__v'
         })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
         .select('-__v')
         .then(dbUser => res.json(dbUser))
         .catch(err => {
@@ -20,6 +24,10 @@ const userController = {
         user.findOne({_id: params.id})
         .populate({
             path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
             select: '-__v'
         })
         .select('-__v')
